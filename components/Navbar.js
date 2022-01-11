b3d2cc43e3867c2f3d87f234ff97f542b696b00d
sstangl33 <stevestangl@gmail.com>
2022-01-11 13:14:07 -0500

    Add mall page.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,16 @@
 import Image from "next/image";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
 import styles from "../styles/Navbar.module.css";
+import { useState } from "react";
 
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.navWrapper}>
@@ -16,21 +25,43 @@ export const Navbar = () => {
             />
           </div>
         </div>
-        <div className={styles.item}>
-          <ul className={styles.list}>
-            <li className={styles.listItem}>
-              <a href="#">Mall Seasonal Photography</a>
+
+        {!isOpen ? (
+          <CloseIcon onClick={toggleMenu} className={styles.menuIcon} />
+        ) : (
+          <MenuIcon onClick={toggleMenu} className={styles.menuIcon} />
+        )}
+
+        <div
+          className={
+            isOpen
+              ? [styles.item, styles.nav, styles.openNav].join(" ")
+              : [styles.item, styles.nav, styles.closedNav].join(" ")
+          }
+        >
+          <div className={styles.logoWrapper}>
+            <Image
+              className={styles.logo}
+              src="/img/edi-logo-2020.png"
+              alt=""
+              width="180px"
+              height="56"
+            />
+          </div>
+          <ul className={styles.navList}>
+            <li className={styles.navListItem}>
+              <a href="./mall">Mall Seasonal Photography</a>
             </li>
-            <li className={styles.listItem}>
+            <li className={styles.navListItem}>
               <a href="#">Sports Photography</a>
             </li>
-            <li className={styles.listItem}>
+            <li className={styles.navListItem}>
               <a href="#">Pre-School Photography</a>
             </li>
-            <li className={styles.listItem}>
+            <li className={styles.navListItem}>
               <a href="#">Families & Seniors</a>
             </li>
-            <li className={styles.listItem}>
+            <li className={styles.navListItem}>
               <a href="#">
                 Contact <br />
                 EDI Imaging
---
+++ pages/mall.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Image from "next/image";
+import Featured from "../components/Featured";
+import Categories from "../components/Categories";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>EDI Imaging</title>
+        <meta
+          name="description"
+          content="EDI Imaging - Premiere Event, Sports, Pre-school, and Portrait Photograpy."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Featured />
+    </div>
+  );
+}
