2ba8240dfb352f1ca96259277183b5732c9c5e8e
sstangl33 <stevestangl@gmail.com>
2022-08-22 14:14:02 -0400

    Fix mistake on the jobs page.

--- components/Thanks.js
+++ components/Thanks.js
@@ -2,24 +2,27 @@ import styles from "../styles/Content.module.css";
 
 const MallContent = () => {
   return (
-    <div className={[styles.container, styles.contact].join(" ")}>
-      <h2
-        className={styles.title}
-        style={{ textTransform: "initial" }}
-      >
-        Thank you for your application!
-      </h2>
-      <p>We will be in contact with you soon.</p>
-    </div>
-    <div className="backBtnContainer">
-    <button
-      type="button"
-      className="button back-btn"
-      onClick={() => handleBackBtn()}
-    >
-      <span className="heading">&lt; Back</span>
-    </button>
-  </div>
+    <>
+      <div className={[styles.container, styles.contact].join(" ")}>
+        <h2
+          className={styles.title}
+          style={{ textTransform: "initial" }}
+        >
+          Thank you for your application!
+        </h2>
+        <p>We will be in contact with you soon.</p>
+      </div>
+
+      <div className="backBtnContainer">
+        <button
+          type="button"
+          className="button back-btn"
+          onClick={() => handleBackBtn()}
+        >
+          <span className="heading">&lt; Back</span>
+        </button>
+      </div>
+    </>
   );
 };
 
