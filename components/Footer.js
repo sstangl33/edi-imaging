bdc954a44207e8bfafc306051cff97e57f6dc78c
Stephen Stangl <stevestangl@gmail.com>
2024-01-25 13:29:04 -0500

    Update date in footer.

--- components/Footer.js
+++ components/Footer.js
@@ -34,7 +34,7 @@ const Footer = () => {
 
         <div className={styles.item}>
           <p className={styles.copyright}>
-            Copyright © 2022 <br /> eDI Imaging <br /> All Rights Reserved.
+            Copyright © 2024 <br /> eDI Imaging <br /> All Rights Reserved.
           </p>
         </div>
       </div>
